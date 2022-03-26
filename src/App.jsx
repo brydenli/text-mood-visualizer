25bc0e780734918c1f025bec364dc7d09c3a4a3e
Bryden Li <brydenli.97@gmail.com>
2022-03-26 16:32:48 -0600

    Fix linting errors

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TextInput from './components/TextInput';
+
+function App() {
+  return (
+    <div>
+      <TextInput />
+    </div>
+  );
+}
+
+export default App;
